93eace2ae6d3e3e30360d7c282a55c4cfb3457a3
Marshall <swolv.lith@gmail.com>
2021-02-10 08:41:30 -0300

    Restoring VM to solve dependency tree problem

--- src/App.js
+++ src/App.js
@@ -1,67 +1,93 @@
-import React, { Component } from 'react'
-import './App.css'
-import Person from './Person/Person'
+import React, { Component } from "react";
+import styled from "styled-components";
+import Person from "./Person/Person";
+import "./App.css";
+
+const StyledBtn = styled.button`
+  background-color: ${(props) => (props.alt ? "red" : "green")};
+  color: white;
+  font: inherit;
+  border: 2px solid #483d8b;
+  box-shadow: 0 2px 3px #8470ff;
+  padding: 8px;
+
+  &:hover {
+    background-color: lightgreen;
+    cursor: pointer;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
     persons: [
-      { id: 'foudfjcf', name: 'Nicco', age: 28 },
-      { id: 'oiecnkjd', name: 'Marshall', age: 25 },
-      { id: 'cmdiofha', name: 'Marcus', age: 23 },
+      { id: "foudfjcf", name: "Nicco", age: 28 },
+      { id: "oiecnkjd", name: "Marshall", age: 25 },
+      { id: "cmdiofha", name: "Marcus", age: 32 },
     ],
-      showPersons: false }
+    showPersons: false,
+  };
 
-  nameChangeHandler = ( event, id ) => {
-    const personIndex = this.state.persons.findIndex( p =>  p.id === id )
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => p.id === id);
 
-    const person = { ...this.state.persons[personIndex] }
-    person.name = event.target.value
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
 
-    const persons = [...this.state.persons]
-    persons[personIndex] = person
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
 
-    this.setState( { persons: persons,
-      showPersons: false } ) }
+    this.setState({ persons: persons, showPersons: false });
+  };
 
   deletePersonHandler = (personIndex) => {
     // .slice() copy the persons state into persons
     // const persons = this.state.persons.slice()
-    const persons = [...this.state.persons]
-    persons.splice(personIndex, 1)
-    this.setState( { persons: persons } )  }
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons
-    this.setState( { showPersons: !doesShow } ) }
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-    let persons = null
+    let persons = null;
 
-    if ( this.state.showPersons ) {
+    if (this.state.showPersons) {
       persons = (
         <div>
-          { this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={ event => this.nameChangeHandler(event, person.id) }
-            />
-          } ) }
-        </div> ) }
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
 
     return (
       <div className="App">
         <h1>Hi, i'm a React App</h1>
         <h2>This is really working!</h2>
-        <button
-          className='button'
-          onClick={this.togglePersonsHandler}>
+        <StyledBtn
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
           Toggle Persons
-        </button>
+        </StyledBtn>
         {persons}
       </div>
-    )  }  }
+    );
+  }
+}
 
-export default App
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import '../App.css'
+import React from "react";
+import styled from "styled-components";
+import "../App.css";
+
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+  @media (max-width: 500px) {
+    width: 450px;
+  }
+`;
 
 const person = (props) => {
- return (
-  <div className="Person">
-    <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p> 
-    <p>{props.children}</p>
-    <input type="text" onChange={props.changed} value={props.name} />
-  </div>
- )
-}
+  return (
+    <StyledDiv>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </StyledDiv>
+  );
+};
 /*  When using class-based components, it's this.props
-  * class Person extends Components {
-  *   render () {
-  *     return <p>My name is {this.props}</p> } }
-*/
-export default person
+ *  class Person extends Components {
+ *   render () {
+ *     return <p>My name is {this.props}</p> } }
+ */
+export default person;
